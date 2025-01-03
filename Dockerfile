5ec47596dad1aa6210d7a805bb292bf16224080a
stuxtwo <dimaac@bk.ru>
2025-01-03 16:39:00 +0100

    Rearanged commands, rearanged docker

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,13 @@ WORKDIR /app
 
 RUN apt-get update && apt-get install -y \
     build-essential \
-    git \
     gcc \
     make \
+    git \
     && rm -rf /var/lib/apt/lists/*
 
 COPY . .
 
-RUN chmod +x controller.sh && ./controller.sh build
+RUN chmod +x controller.sh
 
 CMD ["./controller.sh"]
--- controller.sh
+++ controller.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Function for displaying the prompt and handling commands
+IMAGE_NAME="w1-dimanet"
+
 while true; do
     echo -n "> "
     read cmd 
@@ -16,8 +17,16 @@ while true; do
             ;;
         build)
             echo "Starting Docker containers with docker-compose up --build..."
-            gcc -o main main.c -lm  # Assuming the code is in main.c and you need the math library (-lm)
-            ;;
+            if [[ -f "main.c" ]]; then
+                gcc -o main main.c -lm
+                    if [[ $? -ne 0 ]]; then
+                    echo "Compilation failed."
+                exit 1
+                fi
+            else
+                echo "main.c not found!"
+                exit 1
+            fi
         pull)
             echo "Pulling latest changes from git and docker-compose..."
             git pull
@@ -29,13 +38,17 @@ while true; do
             ;;
         train)
             echo "Training the neural network..."
-            # Assuming 'train_data.txt' and 'model_save' exist or are passed in
             ./dimanet train_data.txt model_save
             ;;
         run)
             echo "Running the neural network..."
-            # Assuming user input is processed and passed to the neural network
-            ./main
+            echo "Running the project..."
+            if [[ -f "./main" ]]; then
+                ./main  # Run the compiled executable
+            else
+                echo "Executable not found. Make sure the build step was successful."
+                exit 1
+            fi
             ;;
         exit)
             echo "Exiting..."
